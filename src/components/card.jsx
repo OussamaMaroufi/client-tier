d1b0e1e78069f0f653deeb7675f4898d527da2db
OussamaMaroufi <oussama.maaroufi.ing@gmail.com>
2022-12-28 23:33:27 +0100

    initial commit

---
+++ src/components/card.jsx
@@ -0,0 +1,39 @@
+import React, { useState} from "react";
+import "./card.css"
+import FormDialog from "./dialog/dialog";
+import axios from "axios";
+
+
+const Card = (props) => {
+    const [open, setOpen] = React.useState(false);
+
+    const cardOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleDeleteGame = () => {
+        axios.delete(`http://Project-ALB-1792821405.us-east-1.elb.amazonaws.com/delete/${props.id}`);
+    }
+
+    return (
+        <>
+        <FormDialog open={open} setOpen={setOpen} id={props.id} name={props.name} cost={props.cost} category={props.category} />
+        <div className="game-card">
+            <div className="info">
+                <h4>{props.name}</h4>
+                <p>${props.cost}</p>
+                <p>{props.category}</p>
+            </div>
+            <div className="actions">
+                <button className="edit" onClick={cardOpen}>Edit</button>
+                <button className="delete" onClick={handleDeleteGame}>Delete</button>
+            </div>
+        </div>
+        </>
+    );
+};
+
+export default Card;
